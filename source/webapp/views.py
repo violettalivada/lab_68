eeeab62bbcac5a4e286e19257a4264625f934b2c
Farid <thesomeq@gmail.com>
2020-07-20 21:57:26 +0600

    Добавлен вывод статей на главную!

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from webapp.models import Article
 
 
 def index_view(request):
-    return render(request, 'index.html')
+    data = Article.objects.all()
+    return render(request, 'index.html', context={
+        'articles': data
+    })
 
 
 def article_create_view(request):

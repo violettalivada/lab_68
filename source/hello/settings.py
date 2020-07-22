324202a925a093b50cc076c112b25668f568712c
Farid <thesomeq@gmail.com>
2020-07-22 19:49:59 +0600

    Добавлено добавление статьи на сайт.

--- source/hello/settings.py
+++ source/hello/settings.py
@@ -106,7 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Bishkek'
+#TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from webapp.models import Article
+from django.http import HttpResponseNotAllowed
 
 
 def index_view(request):
@@ -13,9 +14,12 @@ def article_create_view(request):
     if request.method == "GET":
         return render(request, 'article_create.html')
     elif request.method == 'POST':
-        context = {
-            'title': request.POST.get('title'),
-            'content': request.POST.get('content'),
-            'author': request.POST.get('author')
-        }
+        title = request.POST.get('title')
+        text = request.POST.get('text')
+        author = request.POST.get('author')
+        article = Article.objects.create(title=title, text=text, author=author)
+        context = {'article': article}
         return render(request, 'article_view.html', context)
+    else:
+        return HttpResponseNotAllowed(permitted_methods=['GET', 'POST'])
+

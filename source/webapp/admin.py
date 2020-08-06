fc0fc5fc8a11769343337c06c0486a8c1968a576
Farid <thesomeq@gmail.com>
2020-08-06 20:55:52 +0600

    Поле m-2-m для тегов поменяно на управляемое автоматически.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from webapp.models import Article, Comment, Tag, ArticleTag
+from webapp.models import Article, Comment, Tag
 
 
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    filter_horizontal = ('tags',)
 
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Comment)
 admin.site.register(Tag)
-admin.site.register(ArticleTag)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -14,9 +14,7 @@ class Article(models.Model):
     text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Текст')
     author = models.CharField(max_length=40, null=False, blank=False, default='Unknown', verbose_name='Автор')
     status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='new', verbose_name='Модерация')
-    tags = models.ManyToManyField('webapp.Tag', related_name='articles', through='webapp.ArticleTag',
-                                  through_fields=('article', 'tag'), blank=True, verbose_name='Теги')
-
+    tags = models.ManyToManyField('webapp.Tag', related_name='articles', blank=True, verbose_name='Теги')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
     publish_at = models.DateTimeField(verbose_name="Время публикации", blank=True, default=timezone.now)
@@ -52,11 +50,3 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
-
-
-class ArticleTag(models.Model):
-    article = models.ForeignKey('webapp.Article', related_name='article_tags', on_delete=models.CASCADE, verbose_name='Статья')
-    tag = models.ForeignKey('webapp.Tag', related_name='tag_articles', on_delete=models.CASCADE, verbose_name='Тег')
-
-    def __str__(self):
-        return "{} | {}".format(self.article, self.tag)

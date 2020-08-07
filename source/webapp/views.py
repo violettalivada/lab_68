c49817e35ee536c49288030ea8f4fa56c0e11dd0
Farid <thesomeq@gmail.com>
2020-08-07 19:31:26 +0600

    Поиск по статьям.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -16,6 +16,12 @@ class IndexView(View):
             data = Article.objects.all()
         else:
             data = Article.objects.filter(status='moderated')
+
+        # http://localhost:8000/?search=ygjkjhg
+        search = request.GET.get('search')
+        if search:
+            data = data.filter(title__icontains=search)
+
         return render(request, 'index.html', context={
             'articles': data
         })

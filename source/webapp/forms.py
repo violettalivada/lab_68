94492e6127fd4a1d712ed78bf8c03737d29d5041
Farid <thesomeq@gmail.com>
2020-08-10 20:52:15 +0600

    Добавлены валидаторы на минимальную длину - классовый и не-классовый.

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,7 +1,10 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.core.validators import BaseValidator
+from django.utils.deconstruct import deconstructible
 
 from .models import STATUS_CHOICES, Article, Tag
+
 
 default_status = STATUS_CHOICES[0][0]
 
@@ -9,8 +12,26 @@ default_status = STATUS_CHOICES[0][0]
 BROWSER_DATETIME_FORMAT = '%Y-%m-%dT%H:%M'
 
 
+def at_least_10(string):
+    if len(string) < 10:
+        raise ValidationError('This value is too short!')
+
+
+@deconstructible
+class MinLengthValidator(BaseValidator):
+    message = 'Value "%(value)s" has length of %(show_value)d! It should be at least %(limit_value)d symbols long!'
+    code = 'too_short'
+
+    def compare(self, value, limit):
+        return value < limit
+
+    def clean(self, value):
+        return len(value)
+
+
 class ArticleForm(forms.Form):
-    title = forms.CharField(max_length=200, required=True, label='Заголовок')
+    title = forms.CharField(max_length=200, required=True, label='Заголовок',
+                            validators=[MinLengthValidator(10)])
     text = forms.CharField(max_length=3000, required=True, label='Текст', widget=forms.Textarea)
     author = forms.CharField(max_length=40, required=True, initial='Unknown', label='Автор')
     status = forms.ChoiceField(choices=STATUS_CHOICES, initial=default_status, label='Модерация')
@@ -22,12 +43,6 @@ class ArticleForm(forms.Form):
     # для полей типа DateField
     # publish_at = forms.DateField(..., widget=forms.DateInput(attrs={'type': 'date'}))
     tags = forms.ModelMultipleChoiceField(required=False, label='Теги', queryset=Tag.objects.all())
-
-    def clean_title(self):
-        title = self.cleaned_data['title']
-        if len(title) < 10:
-            raise ValidationError('Title is too short!')
-        return title
 
     def clean(self):
         cleaned_data = super().clean()

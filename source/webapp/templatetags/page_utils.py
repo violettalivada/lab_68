ca9213038221529cb8a5741885326d1ccd895265
Farid <thesomeq@gmail.com>
2020-08-19 21:06:15 +0600

    В шаблоне пагинации выведена вся строка запроса, а не только search.

---
+++ source/webapp/templatetags/page_utils.py
@@ -0,0 +1,11 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter
+def page_query_string(request, page_number):
+    query_args = request.GET.copy()
+    query_args['page'] = page_number
+    return query_args.urlencode()

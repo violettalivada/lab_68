65a4bf7ba64b6a49a08c7662028cdf9134a01863
Farid <thesomeq@gmail.com>
2020-09-03 20:39:04 +0600

    Добавлена регистрация пользователя на кастомной форме.

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from accounts.forms import MyUserCreationForm
 
 
 # def login_view(request):
@@ -19,3 +20,15 @@ from django.contrib.auth import authenticate, login, logout
 # def logout_view(request):
 #     logout(request)
 #     return redirect('index')
+
+
+def register_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        form = MyUserCreationForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = MyUserCreationForm()
+    return render(request, 'user_create.html', context={'form': form})

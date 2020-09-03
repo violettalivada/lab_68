f3ef671d33daa7a6cff367fda8b704e6f51d1150
Farid <thesomeq@gmail.com>
2020-09-03 21:41:20 +0600

    Регистрация пользователя переделана на встроенную форму UserCreationForm и представление на основе CreateView.

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -1,27 +1,10 @@
 from django.contrib.auth.models import User
-from django import forms
+from django.contrib.auth.forms import UserCreationForm, UsernameField
 
 
-class MyUserCreationForm(forms.ModelForm):
-    password = forms.CharField(label="Пароль", strip=False, required=True, widget=forms.PasswordInput)
-    password_confirm = forms.CharField(label="Подтвердите пароль", required=True, widget=forms.PasswordInput, 
-                                       strip=False)
-
-    def clean(self):
-        cleaned_data = super().clean()
-        password = cleaned_data.get("password")
-        password_confirm = cleaned_data.get("password_confirm")
-        if password and password_confirm and password != password_confirm:
-            raise forms.ValidationError('Пароли не совпадают!')
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password"])
-        if commit:
-            user.save()
-        return user
-
+class MyUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'password', 'password_confirm',
+        fields = ['username', 'password1', 'password2',
                   'first_name', 'last_name', 'email']
+        field_classes = {'username': UsernameField}
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,5 +1,9 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from django.views.generic import CreateView
+
 from accounts.forms import MyUserCreationForm
 
 
@@ -22,13 +26,20 @@ from accounts.forms import MyUserCreationForm
 #     return redirect('index')
 
 
-def register_view(request, *args, **kwargs):
-    if request.method == 'POST':
-        form = MyUserCreationForm(data=request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('index')
-    else:
-        form = MyUserCreationForm()
-    return render(request, 'user_create.html', context={'form': form})
+class RegisterView(CreateView):
+    model = User
+    template_name = 'user_create.html'
+    form_class = MyUserCreationForm
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if not next_url:
+            next_url = self.request.POST.get('next')
+        if not next_url:
+            next_url = reverse('index')
+        return next_url

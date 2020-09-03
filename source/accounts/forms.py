4eaf39a8ca51d0ff06460d78920d0528e2e3c19a
Farid <thesomeq@gmail.com>
2020-09-03 21:43:41 +0600

    Класс Meta в форме регистрации наследован от класса UserCreationForm.Meta, чтобы упростить код и уменьшить количество импортов.

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -1,10 +1,7 @@
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm, UsernameField
+from django.contrib.auth.forms import UserCreationForm
 
 
 class MyUserCreationForm(UserCreationForm):
-    class Meta:
-        model = User
+    class Meta(UserCreationForm.Meta):
         fields = ['username', 'password1', 'password2',
                   'first_name', 'last_name', 'email']
-        field_classes = {'username': UsernameField}

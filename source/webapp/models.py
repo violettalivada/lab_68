831100d892850bab1c212857946bc9090c9e8c58
Farid <thesomeq@gmail.com>
2020-09-07 21:00:52 +0600

    Авторы комментариев и статей переделаны на внешние ключи к пользователю.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.core.validators import MinLengthValidator
 from django.db import models
 from django.utils import timezone
@@ -14,7 +15,8 @@ class Article(models.Model):
     title = models.CharField(max_length=200, null=False, blank=False, verbose_name='Заголовок',
                              validators=[MinLengthValidator(10)])
     text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Текст')
-    author = models.CharField(max_length=40, null=False, blank=False, default='Unknown', verbose_name='Автор')
+    author = models.ForeignKey(get_user_model(), on_delete=models.SET_DEFAULT, default=1,
+                               related_name='articles', verbose_name='Автор')
     status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='new', verbose_name='Модерация')
     tags = models.ManyToManyField('webapp.Tag', related_name='articles', blank=True, verbose_name='Теги')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
@@ -41,7 +43,8 @@ class Comment(models.Model):
     article = models.ForeignKey('webapp.Article', related_name='comments',
                                 on_delete=models.CASCADE, verbose_name='Статья')
     text = models.TextField(max_length=400, verbose_name='Комментарий')
-    author = models.CharField(max_length=40, null=True, blank=True, default='Аноним', verbose_name='Автор')
+    author = models.ForeignKey(get_user_model(), on_delete=models.SET_DEFAULT, default=1,
+                               related_name='comments', verbose_name='Автор')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
 

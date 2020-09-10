ce07a3d08aaf54ce2c1bfd3ea8e632ba962acaa7
Farid <thesomeq@gmail.com>
2020-09-10 19:40:28 +0600

    Добавлена модель профиля для пользователя.

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -39,3 +39,17 @@ class AuthToken(models.Model):
     class Meta:
         verbose_name = 'Аутентификационный токен'
         verbose_name_plural = 'Аутентификационные токены'
+
+
+class Profile(models.Model):
+    user: AbstractUser = models.OneToOneField(get_user_model(), related_name='profile',
+                                              on_delete=models.CASCADE, verbose_name='Пользователь')
+    birth_date = models.DateField(null=True, blank=True, verbose_name='Дата рождения')
+    avatar = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Аватар')
+
+    def __str__(self):
+        return self.user.get_full_name() + "'s Profile"
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'

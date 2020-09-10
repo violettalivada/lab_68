24c2f5a7df49e185af29e5b5653a52238418c3dc
Farid <thesomeq@gmail.com>
2020-09-10 22:49:31 +0600

    Добавлена пагинация статей пользователя и вывод фразы "Нет статей" в списке статей, если статей нет.

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
@@ -74,3 +75,15 @@ class UserDetailView(LoginRequiredMixin, DetailView):
     model = get_user_model()
     template_name = 'user_detail.html'
     context_object_name = 'user_obj'
+    paginate_related_by = 5
+    paginate_related_orphans = 0
+
+    def get_context_data(self, **kwargs):
+        articles = self.object.articles.order_by('-created_at')
+        paginator = Paginator(articles, self.paginate_related_by, orphans=self.paginate_related_orphans)
+        page_number = self.request.GET.get('page', 1)
+        page = paginator.get_page(page_number)
+        kwargs['page_obj'] = page
+        kwargs['articles'] = page.object_list
+        kwargs['is_paginated'] = page.has_other_pages()
+        return super().get_context_data(**kwargs)
